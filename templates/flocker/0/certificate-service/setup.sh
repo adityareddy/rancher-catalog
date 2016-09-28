6f99f031b37186dbd91d3c82fec508640f474123
adityareddy <p.aditya.reddi@gmail.com>
2016-09-28 19:23:22 +0530

    evetv

---
+++ templates/flocker/0/certificate-service/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+META_URL="http://rancher-metadata/2015-07-25"
+SELF_NAME=$(curl -s -H 'Accept: application/json' ${META_URL}/self/host| jq -r .name)
+
+export PATH=$PATH:/opt/flocker/bin/
+
+rm /etc/flocker/*
+cp /app/agent.yml /etc/flocker/agent.yml
+echo -e "\n  hostname: \"${SELF_NAME}\"" >> /etc/flocker/agent.yml
+curl -LOk https://s3.ap-south-1.amazonaws.com/adityareddy-rancher/certs/cluster.crt
+curl -LOk https://s3.ap-south-1.amazonaws.com/adityareddy-rancher/certs/cluster.key
+# if [ "$NODE" = "CONTROL" ]
+# then
+    flocker-ca create-control-certificate $(hostname) 
+    rename 's/[^.]*/control-service/' control*
+# fi
+flocker-ca create-node-certificate
+for OUTPUT in $(ls -Art | tail -n 2)
+do
+  echo $OUTPUT
+	mv $OUTPUT ${OUTPUT/*./node.}
+done
+chmod 0600 /etc/flocker/*
