e16cca0b45b51b994a3785f2e0db6a99154845cb
root <p.aditya.reddi@gmail.com>
2016-09-30 14:21:42 +0000

    scf

--- templates/flocker/0/certificate-service/setup.sh
+++ templates/flocker/0/certificate-service/setup.sh
@@ -5,16 +5,16 @@ CONTROL_SERVICE_IP=$(curl -s -H 'Accept: application/json' ${META_URL}/stacks/fl
 
 export PATH=$PATH:/opt/flocker/bin/
 
-rm /etc/flocker/*
+rm -r /etc/flocker/*
 cp /app/agent.yml /etc/flocker/agent.yml
-echo -e "\n  hostname: ${SELF_NAME} >> /etc/flocker/agent.yml
+echo -e "\n  hostname: ${CONTROL_SERVICE_IP}" >> /etc/flocker/agent.yml
+
 curl -LOk https://s3.ap-south-1.amazonaws.com/adityareddy-rancher/certs/cluster.crt
 curl -LOk https://s3.ap-south-1.amazonaws.com/adityareddy-rancher/certs/cluster.key
-# if [ "$NODE" = "CONTROL" ]
-# then
-    flocker-ca create-control-certificate $(hostname) 
-    rename 's/[^.]*/control-service/' control*
-# fi
+
+flocker-ca create-control-certificate $(hostname) 
+rename 's/[^.]*/control-service/' control*
+
 flocker-ca create-node-certificate
 for OUTPUT in $(ls -Art | tail -n 2)
 do
@@ -25,3 +25,4 @@ chmod 0600 /etc/flocker/*
 
 git clone https://github.com/adityareddy/lunanode-flocker-plugin
 /opt/flocker/bin/pip install lunanode-flocker-plugin/
+/opt/flocker/bin/pip list | grep -i huawei-oceanstor-flocker-plugin
